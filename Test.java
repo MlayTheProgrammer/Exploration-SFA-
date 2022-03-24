d42be08286a79a64c189abd542c120af442db542
CarlHawley <carlhawley@gmail.com>
2022-03-24 00:51:04 -0500

    fixed count bug, added step val to make getting our format less effort

--- Test.java
+++ Test.java
@@ -12,7 +12,8 @@ public class Test
     public static void main(String[] args)
     {
         int START = 0;
-        int END = 5000;
+        int END = 5001;
+        int STEP = 100;
         long start_time, stop_time, over_time, time;
         Random r = new Random();
         // running once to eliminate first-time-run bug
@@ -20,11 +21,13 @@ public class Test
         Bubblesort.bubblesort(new int[]{1, 2});
         //table header
         System.out.format("%10s\t%5s\t%20s\t%25s\n", "TYPE", "INDEX", "TIME(ns)", "# OF BASIC OPERATIONS");
-        for(int n = START; n< END; n++)
+        for(int n = START; n< END; n+=STEP)
         {
             start_time = System.nanoTime();
             stop_time = System.nanoTime();
             over_time = stop_time - start_time;
+            Mergesort.count=0;
+            Bubblesort.count=0;
 
             //generate 2 duplicate arrays of length n
             int[] arr1 = new int[n];
